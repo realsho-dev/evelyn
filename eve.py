e9a84557bafa4383c45cb00039b9a0c62748e618
sho <168981721+realsho-dev@users.noreply.github.com>
2025-08-13 16:12:02 +0530

    Update eve.py

--- eve.py
+++ eve.py
@@ -5,6 +5,10 @@ from together import Together
 import discord
 from discord.ext import commands
 
+# Healthcheck
+import healthcheck
+healthcheck.start()
+
 # Load env
 load_dotenv()
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
